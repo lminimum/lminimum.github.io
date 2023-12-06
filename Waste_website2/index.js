49f1501d3b3c9e1ee89540b86537e8ad2805990f
Lachille <742417553@qq.com>
2023-12-07 00:15:59 +0800

    renew

---
+++ Waste_website2/index.js
@@ -0,0 +1,17 @@
+// const container = document.querySelectorAll('.container')
+
+// container.forEach(container => {
+//     container.addEventListener('click', () => {
+//         removeActiveClasses()
+//         panel.classList.add('active')
+//     })
+// })
+
+// function removeActiveClasses() {
+//     container.forEach(container => {
+//         container.classList.remove('active')
+//     })
+// }
+function themeaction(){
+    
+}
